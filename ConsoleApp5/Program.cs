6bf06a835b9e7835c688d3c549188942080bed4a
Mark Rosenberg <mrosenberg@prscholas.org>
2021-04-28 14:55:59 -0500

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NORTHWNDEntities ent = new NORTHWNDEntities())
+            {
+                foreach(var item in ent.Categories.ToList())
+                {
+                    Console.WriteLine(item.CategoryName);
+                }
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
